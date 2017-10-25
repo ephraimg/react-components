f8e332475ae41963cc30419e1d18931346acdc49
ephraimg <ephraim.glick@gmail.com>
2017-10-25 11:28:20 -0700

    Complete react-components

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+var GroceryList = ({ items }) => (
+    <ul>
+      {items.map(item => 
+        <GroceryListItem item={item} key={item} />
+      )}
+    </ul>
+  );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false
+    }
+  }
+  onListItemHover(event) {
+    this.setState({
+      hovered: !this.state.hovered
+    })
+  }
+  render() {
+    var style = { 
+      fontWeight: this.state.hovered ? '800' : '400'
+    };
+    return (
+      <li style={style}>
+        <span onMouseOver={this.onListItemHover.bind(this)}
+          onMouseOut={this.onListItemHover.bind(this)}>
+          {this.props.item}
+        </span>
+      </li>
+    )
+  }
+} 
+
+ReactDOM.render(<GroceryList items={['Beer', 'Rum', 'Potatoes']}/>, document.getElementById("app"));
